815bc3cab0ba659f940fc443cbc2f72fa43555bf
XayahSuSuSu <zds1249475336@gmail.com>
2022-04-10 01:36:10 +0800

    build: Add modular build hooks
    * Ref: https://github.com/ProtonAOSP/android_vendor_proton
    
    Change-Id: If8ae32b04f674fbf5af37c4f8f84bbba390cb523

---
+++ build/init.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 Zody UI
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Acme version
+-include vendor/zody/build/tools/version.mk
---
+++ build/tools/version.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2022 Zody UI
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ZODY_VERSION := sense
+
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.build.version.custom=$(ZODY_VERSION)
