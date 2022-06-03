AcmeUI/android_vendor_acme

config: Export GMS prop
Change-Id: I15991e631557f2d4a04e6d2b1257cd55c31b268a

config/version.mk
@@ -19,3 +19,14 @@ ACME_VERSION := AcmeUI-$(ACME_BRANCH)-$(ACME_DEVICE)-$(ACME_BUILD_TYPE)-$(ACME_D
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.acme.branch=$(shell echo "$(ACME_BRANCH)") \
     ro.acme.build.type=$(shell echo "$(ACME_BUILD_TYPE)")
+
+ifeq ($(WITH_GMS), true)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.acme.gms.type=GMS
+else ifeq ($(WITH_GMS_CORE), true)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.acme.gms.type=GMSCore
+else
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.acme.gms.type=VANILLA
+endif

Zody-UI/android_vendor_zody

config/version.mk
@@ -36,3 +36,11 @@ ZODY_DISPLAY_VERSION := ZodyUI-$(ZODY_BRANCH)-v$(ZODY_VERISONCODE)-$(ZODY_DEVICE
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.zody.version=$(ZODY_VERSION) \
     ro.zody.build.version=$(ZODY_DISPLAY_VERSION)
+
+ifeq ($(ZODY_WITH_GMS), true)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.zody.gms.type=GMS
+else
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.zody.gms.type=VANILLA
+endif