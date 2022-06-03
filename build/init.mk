CSC-UI/android_vendor_csc

overlay: Add product overlay
* We're going to move most of our config changes here for portability.

build/init.mk
@@ -6,6 +6,7 @@
 
 # Inherit vendor submodules
 $(call inherit-product, vendor/acme/bootanimation/bootanimation.mk)
+$(call inherit-product, vendor/acme/overlay/overlay.mk)
 
 # Acme version
 -include vendor/acme/build/tools/version.mk

Zody-UI/android_vendor_zody

Change-Id: I4094c08451858e7ba668f50cf5bb4256ee3fef40

build/init.mk
@@ -6,6 +6,7 @@
 
 # Inherit vendor submodules
 $(call inherit-product, vendor/zody/bootanimation/bootanimation.mk)
+$(call inherit-product, vendor/zody/overlay/overlay.mk)
 
 # Acme version
 -include vendor/zody/build/tools/version.mk