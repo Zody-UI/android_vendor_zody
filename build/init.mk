CSC-UI/android_vendor_csc

build: Inherit our bootanimation

build/init.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit vendor submodules
+$(call inherit-product, vendor/acme/bootanimation/bootanimation.mk)
+
 # Acme version
 -include vendor/acme/build/tools/version.mk

Zody-UI/android_vendor_zody

Change-Id: I2860907b421666758d53458ddb89074aff1dbc7b

build/init.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit vendor submodules
+$(call inherit-product, vendor/zody/bootanimation/bootanimation.mk)
+
 # Acme version
 -include vendor/zody/build/tools/version.mk