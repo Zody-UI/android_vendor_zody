CSC-UI/android_vendor_csc

config: Add basic vendor
Co-Authored-By: XpLoDWilD <1205428+xplodwild@users.noreply.github.com>
Co-Authored-By: TH779 <30104958+hh2333@users.noreply.github.com>

config/version.mk
@@ -4,7 +4,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-ACME_VERSION := 12.1
+# Version
+ACME_BRANCH := spring
+ACME_BUILD_TYPE := DEBUG
+ACME_DATE := $(shell date -u +%Y%m%d)
+ACME_DEVICE := $(shell echo "$(TARGET_PRODUCT)" | cut -d '_' -f 2,3)
 
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.build.version.custom=$(ACME_VERSION)
+ifeq ($(IS_RELEASE), true)
+    ACME_BUILD_TYPE := RELEASE
+endif
+
+ACME_VERSION := AcmeUI-$(ACME_BRANCH)-$(ACME_DEVICE)-$(ACME_BUILD_TYPE)-$(ACME_DATE)
+ACME_DISPLAY_VERSION := AcmeUI-$(ACME_BRANCH)-$(ACME_DATE)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.acme.version=$(ACME_VERSION) \
+    ro.acme.build.version=$(ACME_DISPLAY_VERSION)

Zody-UI/android_vendor_zody

Change-Id: I7bd2f87411cd29a00e0c1e207f3886a33f24250a

config/version.mk
@@ -4,7 +4,35 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-ZODY_VERSION := sense
+# Version
+ZODY_BRANCH := sense
+ZODY_VERISONCODE := 1.0.0
+ZODY_BUILD_TYPE := BETA
+ZODY_COMPILER_TYPE := UNOFFICIAL
+ZODY_DATE := $(shell date -u +%Y%m%d)
+ZODY_DEVICE := $(shell echo "$(TARGET_PRODUCT)" | cut -d '_' -f 2,3)
+ZODY_GMS := Vanilla
+ZODY_HMS :=
 
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.build.version.custom=$(ZODY_VERSION)
+ifeq ($(ZODY_IS_RELEASE), true)
+    ZODY_BUILD_TYPE := RELEASE
+endif
+
+ifeq ($(ZODY_OFFICIAL_IS_CHECKED), true)
+    ZODY_COMPILER_TYPE := OFFICIAL
+endif
+
+ifeq ($(ZODY_WITH_GMS), true)
+    ZODY_GMS := GMS
+endif
+
+ifeq ($(ZODY_WITH_HMS), true)
+    ZODY_HMS := +HMS
+endif
+
+ZODY_VERSION := ZodyUI-$(ZODY_BRANCH)-v$(ZODY_VERISONCODE)-$(ZODY_DEVICE)-$(ZODY_DATE)-$(ZODY_BUILD_TYPE)-$(ZODY_COMPILER_TYPE)-$(ZODY_GMS)$(ZODY_HMS)
+ZODY_DISPLAY_VERSION := ZodyUI-$(ZODY_BRANCH)-v$(ZODY_VERISONCODE)-$(ZODY_DEVICE)-$(ZODY_DATE)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.zody.version=$(ZODY_VERSION) \
+    ro.zody.build.version=$(ZODY_DISPLAY_VERSION)